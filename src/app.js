768975f2cb9ebcb4ee3cc51aa80137d25d163e2d
Tony Gonzalez <tonylucasgonzalez@gmail.com>
2020-01-12 23:16:23 -0300

    Third part

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import youtube from './api/youtube';
-import  { SearchBar, VideoDetail}  from './components';
+import  { SearchBar, VideoDetail, VideoList}  from './components';
 
 
 class App extends React.Component { 
@@ -23,7 +23,7 @@ handleSubmit = async (searchTerm) => {
 }
 
     render() { 
-        const { selectedVideo } = this.state;
+        const { selectedVideo, videos } = this.state;
         return(
             <Grid justifiy="center" container spacing= {10}>
                 <Grid item xs={12}>
@@ -35,7 +35,7 @@ handleSubmit = async (searchTerm) => {
                             </Grid>
                             <VideoDetail video={selectedVideo}/>
                             <Grid item xs = {4}>
-                            {/* VIDEO LIST */}
+                            <VideoList videos={videos} />
                          </Grid>
                     </Grid>
                  </Grid>
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
-const VideoDetail = () => {
+import { Paper, Typography } from '@material-ui/core';
+
+const VideoDetail = ({ video }) => {
+
+    if(!video) return <div> Loading... </div>
+
+    console.log(video);
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
     return (
-        <h1>This is a Video Detail Component</h1> 
+        <React.Fragment> 
+            <Paper elevation = {6} style={{height: '70%'}}> 
+            <iframe frameBorder= "0" height="100%" width="100%" title="Video Player" src={videoSrc} />
+
+            </Paper>
+            <Paper elevation = {6} style ={{ padding: '15px'}}>
+            <Typography variant = "h4">{video.snippet.title} - {video.snippet.channelTitle} </Typography>
+            <Typography variant = "subtitle1"> {video.snippet.channel}  </Typography>
+            <Typography variant = "subtitle2"> {video.snippet.description} </Typography>
+
+            <Typography> </Typography>
+
+            </Paper>
+
+        </React.Fragment> 
+        
     )
 }
 
